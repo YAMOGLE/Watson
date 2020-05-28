2b13a5da494e65b6afe69c59a7a2c9638d203af4
Molin <molin.yang.gr@dartmouth.edu>
2020-05-28 14:17:27 -0400

    First commit

---
+++ app/src/main/java/com/example/watson/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.watson;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity implements IntroFragment.OnFragmentInteractionListener , ClassifierFragment.OnFragmentInteractionListener , DetailsFragment.OnFragmentInteractionListener, SuggestionsFragment.OnFragmentInteractionListener{
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new IntroFragment();
+                    break;
+                case R.id.navigation_chart:
+                    fragment = new ClassifierFragment();
+                    break;
+                case R.id.navigation_detail:
+                    fragment = new DetailsFragment();
+                    break;
+                case R.id.navigation_suggestion:
+                    fragment = new SuggestionsFragment();
+                    break;
+            }
+            if(fragment == null) return false;
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+            return true;
+        }
+    };
+
+    public void onFragmentInteraction(Uri uri){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, new IntroFragment()).commit();
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
